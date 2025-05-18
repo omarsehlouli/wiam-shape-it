199bf8cadfa40e7f0323d5bba5544c961c2c9f71
Your Name <anonymous@example.com>
2025-05-18 19:41:08 +0100

    lol news

--- app/a-propos/page.tsx
+++ app/a-propos/page.tsx
@@ -40,7 +40,7 @@ export default function AProposPage() {
             <div className="relative h-[500px] rounded-lg overflow-hidden shadow-xl">
               <Image 
                 src="/gym_entrance.webp"
-                alt="Shape It Studio"
+                alt="Shape It"
                 fill
                 className="object-cover"
               />
